025896a04a1629e8942154e654c51ca7bf05d897
Kacper Sadowski <148885257+Sadowski123@users.noreply.github.com>
2023-11-06 16:28:05 +0100

    Add files via upload

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,50 @@
 #pragma once
-
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public: 
+        ResourceManager()  // konstruktor domyœlny
+        {
+        	cos = new Resource();
+		}
+        
+        double get()  //metoda double get
+        {
+        	return cos->get();
+		}
+        
+        ResourceManager(const ResourceManager& i)  // konstruktor kopiuj¹cy
+        {
+        	cos = new Resource();
+        	*cos = *i.cos;	//kopiujê wartoœæ
+		}
+        
+    	ResourceManager& operator=(const ResourceManager& i)  //kopiuj¹cy operator przypisania
+    	{
+    		*cos = *i.cos;
+    		return *this;  //gdy korzystamy z operatora przypisania korzystamy z return 
+		}
+		
+		ResourceManager (ResourceManager&& i)  //konstruktor przenosz¹cy
+		{
+			cos = i.cos;
+			i.cos = nullptr;  //nullptr to wskaŸnik pusty
+		}
+		
+		ResourceManager& operator=(ResourceManager&& i)  //przenosz¹cy operator przypisania
+    	{
+    		delete cos;
+    		cos = i.cos;
+    		i.cos = nullptr;
+    		return *this;
+		}
+		
+		~RecourceManager()
+		{
+			delete cos;
+		}
+		
+    private: 
+        Resource* cos; 
 };
